0a90ab365c1422a6e9924e49fd1675c5172f5003
jac <joao.coutinho@sebasi.pt>
2023-09-15 16:57:49 +0100

    feat: sender service created

---
+++ src/main/java/com/jaoc/emailservice/application/EmailSenderService.java
@@ -0,0 +1,25 @@
+package com.jaoc.emailservice.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jaoc.emailservice.adapters.EmailSenderGateway;
+import com.jaoc.emailservice.core.EmailSenderUseCase;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway) {
+        this.emailSenderGateway = emailGateway;
+
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+    
+}
